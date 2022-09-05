9f97239c583107c711824b7a1c9d5e090d30a5fc
FACorreiaa <fernandocorreia316@gmail.com>
2022-09-05 23:37:37 +0200

    continous median

---
+++ continuousMedian.py
@@ -0,0 +1,84 @@
+class ContinuousMedianHandler:
+    def __init__(self):
+        self.lowers = Heap(MAX_HEAP_FUNC, [])
+        self.greaters = Heap(MIN_HEAP_FUNC, [])
+        self.median = None
+    # O(log(n)) time | O(n) space
+
+    def insert(self, number):
+        if not self.lowers.length or number < self.lowers.peek():
+            self.lowers.insert(number)
+        else:
+            self.greaters.insert(number)
+        self.rebalanceHeaps()
+        self.updateMedian()
+
+    def rebalanceHeaps(self):
+        if self.lowers.length - self.greaters.length == 2:
+            self.greaters.insert(self.lowers.remove())
+        elif self.greaters.length - self.lowers.length == 2:
+            self.lowers.insert(self.greaters.remove())
+
+    def updateMedian(self):
+        if self.lowers.length == self.greaters.length:
+            self.median = (self.lowers.peek() + self.greaters.peek()) / 2
+        elif self.lowers.length == self.greaters.length:
+            self.median = self.lowers.peek()
+        else:
+            self.median = self.greaters.peek()
+
+    def getMedian(self):
+        return self.median
+
+
+class Heap:
+    def __init__(self, array):
+        self.heap = self.buildHeap(array)
+    # O(n) time | O(1) space
+
+    def buildHeap(self, array):
+        firstParentIdx = (len(array) - 2) // 2
+        for currentIdx in reversed(range(firstParentIdx)):
+            self.siftDown(currentIdx, len(array) - 1, array)
+        return array
+
+    # O(log(n)) time | O(1) space
+    def siftDown(self, currentIdx, endIdx, heap):
+        childOneIdx = currentIdx * 2 + 1
+        while childOneIdx <= endIdx:
+            childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 < endIdx else -1
+            if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+                idxToSwap = childTwoIdx
+            else:
+                idxToSwap = childOneIdx
+            if heap[idxToSwap] < heap[currentIdx]:
+                self.swap(currentIdx, idxToSwap, heap)
+                currentIdx = idxToSwap
+                childOneIdx = currentIdx * 2 + 1
+            else:
+                break
+    # O(log(n)) time | O(1) space
+
+    def siftUp(self, currentIdx, heap):
+        parentIdx = (currentIdx - 1) // 26
+        while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
+            self.swap(currentIdx, parentIdx, heap)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) // 2
+
+    def peek(self):
+        return self.heap[0]
+
+    def swap(self, i, j, heap):
+        heap[i], heap[j] = heap[j], heap[i]
+
+    # O(log(n)) time | O(1) space
+    def remove(self):
+        swap(0, len(self.heap) - 1, self.heap)
+        valueToRemove = self.heap.pop()
+        self.siftDown(0, len(self.heap) - 1, self.heap)
+        return valueToRemove
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp(len(self.heap) - 1, self.heap)
