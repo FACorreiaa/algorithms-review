c92a13a569b7c91b80e47d1d27126c74a5f4df0e
FACorreiaa <fernandocorreia316@gmail.com>
2022-09-08 09:15:42 +0200

    shifted binary search algo

---
+++ shiftedBinarySearch.py
@@ -0,0 +1,48 @@
+# def shiftedBinarySearch(array, target):
+#     return shiftedBinarySearchHelper(array, target, 0, len(array) - 1)
+
+# O(log N) time | O(log(N)) space
+# def shiftedBinarySearchHelper(array, target, left, right):
+#     if left > right:
+#         return -1
+#     middle = (left, right) // 2
+#     potentialMatch = array[middle]
+#     leftNum = array[left]
+#     rightNum = array[right]
+#     if target == potentialMatch:
+#         return middle
+#     elif leftNum <= potentialMatch:
+#         if target < potentialMatch and target >= leftNum:
+#             return shiftedBinarySearchHelper(array, target, left, middle - 1)
+#         else:
+#             return shiftedBinarySearchHelper(array, target, middle + 1, right)
+#     else:
+#         if target > potentialMatch and target <= rightNum:
+#             return shiftedBinarySearchHelper(array, target, middle + 1, right)
+#         else:
+#             return shiftedBinarySearchHelper(array, target, left, middle - 1)
+
+# O(log N) time | O(1) space
+def shiftedBinarySearch(array, target):
+    return shiftedBinarySearchHelper(array, target, 0, len(array) - 1)
+
+
+def shiftedBinarySearchHelper(array, target, left, right):
+    while left <= right:
+        middle = (left, right) // 2
+        potentialMatch = array[middle]
+        leftNum = array[left]
+        rightNum = array[right]
+        if target == potentialMatch:
+            return middle
+        elif leftNum <= potentialMatch:
+            if target < potentialMatch and target >= leftNum:
+                right = middle - 1
+            else:
+                left = middle + 1
+        else:
+            if target > potentialMatch and target <= rightNum:
+                left = middle + 1
+            else:
+                right = middle - 1
+    return -1
