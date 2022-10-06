357e2d3016f1a70f224bc224977b62e5633ddd95
FACorreiaa <fernandocorreia316@gmail.com>
2022-10-07 00:11:45 +0200

    lru cache algo

--- lruCache.py
+++ lruCache.py
@@ -1,3 +1,45 @@
+class LRUCache:
+    def __init__(self, maxSize):
+        self.cache = {}
+        self.maxSize = maxSize or 1
+        self.currentSize = 0
+        self.listOfMostRecent = DoubleLinkedList()
+
+    # O(1) Time | O(1) Space
+    def insertKeyValuePair(self, key, value):
+        if key not in self.cache:
+            if self.currentSize == self.maxSize:
+                self.evictLastRecent()
+            else:
+                self.currentSize += 1
+            self.cache[key] = DoubleLinkedList(key, value)
+        else:
+            self.replaceKey(key, value)
+        self.updateMostRecent(self.cache[key])
+
+    def getValueFromKey(self, key):
+        if key not in self.cache:
+            return None
+        self.updateMostRecent(self.cache[key])
+        return self.cache[key].value
+
+    def getMostRecentKey(self):
+        return self.listOfMostRecent.head.key
+
+    def evictLastRecent(self):
+        keyToRemove = self.listOfMostRecent.tail.key
+        self.listOfMostRecent.remove()
+        del self.cache[keyToRemove]
+
+    def updateMostRecent(self, node):
+        self.listOfMostRecent.setHead(node)
+
+    def replaceKey(self, key, value):
+        if key not in self.cache:
+            raise Exception("The provided key isnt in the cache")
+        self.cache[key].value = value
+
+
 class DoubleLinkedList:
     def __init__(self):
         self.heaf = None
